a50d5dfeeca2bb6ce81702aa74d4ec5289f9fca4
Kasilnia <kas.pieczynska@gmail.com>
2018-01-03 15:58:21 +0100

    Add displayig data in html

---
+++ display_data.py
@@ -0,0 +1,36 @@
+import json
+import webbrowser
+
+
+offers_html = open('offers.html', 'w')
+content = '<html><head></head><body><table>'
+data = json.load(open('offers.json'))
+
+content += """
+   <tr>
+       <th>Dealer name</th>
+       <th>Offer url</th>
+       <th>Car name</th>
+       <th>Price</th>
+   </tr>
+"""
+
+for dealer in data['dealers']:
+    for i, offer in enumerate(dealer['offers']):
+        if i == 0:
+            offer_rows = '<tr><td>{dealer}</td>'.format(
+                dealer=dealer['dealer_name'])
+        else:
+            offer_rows = '<tr><td></td>'
+        offer_rows += '<td>' + offer['offer_url'] + '</td>'
+        offer_rows += '<td>' + offer['car_name'] + '</td>'
+        offer_rows += '<td>' + offer['price'] + '</td>'
+        content += '{offer}</tr>'.format(offer=offer_rows)
+
+
+content += '</table></body></html>'
+
+offers_html.write(content)
+offers_html.close()
+
+webbrowser.open_new_tab('offers.html')
